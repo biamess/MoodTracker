e5c3372659c60731176a61130ce05ec105358f41
biamess <12700458+biamess@users.noreply.github.com>
2020-03-25 15:21:51 -0400

    don't include future dates in the current month in mood table

--- MoodTracker/Controllers/DailyMoodController.cs
+++ MoodTracker/Controllers/DailyMoodController.cs
@@ -25,7 +25,7 @@ namespace MoodTracker.Controllers
         {
             DailyMoodIndexViewModel vm = new DailyMoodIndexViewModel
             {
-                Dates = GetDatesInYear(DateTime.Now.Year, DateTime.Now.Month),
+                Dates = GetDatesInYear(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day),
 
                 DailyMoods = await _context.DailyMoods
                 .Include(d => d.Mood)
@@ -188,12 +188,13 @@ namespace MoodTracker.Controllers
         /// </summary>
         /// <param name="year">Year</param>
         /// <param name="month">Month (0-11)</param>
-        /// <returns><list type="<list<DateTime>>">list</list> containing all the dates of the past year.</returns>
-        public static List<List<DateTime>> GetDatesInYear(int year, int month)
+        /// <returns><list type="<list<DateTime>>">list</list> containing the dates of the past 12 months (not including future dates this month).</returns>
+        public static List<List<DateTime>> GetDatesInYear(int year, int month, int day)
         {
             List<List<DateTime>> dates = new List<List<DateTime>>();
             int currMonth = month + 1;
             int currYear = year - 1;
+            int lastDay;
 
             for (int i=1; i<=12; i++)
             {
@@ -202,18 +203,27 @@ namespace MoodTracker.Controllers
                     currMonth = 1;
                     currYear++;
                 }
-                dates.Add(GetDatesInMonth(currYear, currMonth));
+
+                lastDay = (currYear == year && currMonth == month) ? day : DateTime.DaysInMonth(currYear, currMonth);
+                dates.Add(GetDatesInMonth(currYear, currMonth, lastDay));
 
                 currMonth += 1;
             }
             return dates;
         }
 
-        public static List<DateTime> GetDatesInMonth(int year, int month)
+        /// <summary>
+        /// Get a <list type="<list<DateTime>>"> of days in the given month and year, up to the given day.
+        /// </summary>
+        /// <param name="year">Year to get dates for.</param>
+        /// <param name="month">Month to get dates for.</param>
+        /// <param name="day">Ending day for the range of dates.</param>
+        /// <returns><list type="<list<DateTime>>"> of days in the given month and year, up to the given day.</returns>
+        public static List<DateTime> GetDatesInMonth(int year, int month, int day)
         {
-            return Enumerable.Range(1, DateTime.DaysInMonth(year, month))  // Days: 1, 2 ... 31 etc.
+            return Enumerable.Range(1, day)  // Days: 1, 2 ... day
                              .Select(day => new DateTime(year, month, day)) // Map each day to a date
-                             .ToList(); // Load dates into a list
+                             .ToList();
         }
 
         public async Task<DailyMoodViewModel> GetDailyMoodViewModel(int dailyMoodId)
